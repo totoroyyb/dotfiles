3cc2474b092e744f0f29af97f15838527f410ce1
Yibo Yan <yiboyan@usc.edu>
2024-01-13 22:03:04 -0700

    mods

---
+++ run_once_before_env-setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function echo_to_file {
+    local line="$1"
+    local file="$2"
+
+    if grep -Fxq "$line" "$file"; then
+	echo "Found $line in file: $file"
+    else
+	echo $line >> $file
+    fi
+}
+
+BASH_CONF_PATH="$HOME/.bash_profile"
+ZSH_CONF_PATH="$HOME/.zshrc"
+
+function echo_to_conf {
+    echo_to_file "$1" "$BASH_CONF_PATH"
+    echo_to_file "$1" "$ZSH_CONF_PATH"
+}
+
+EDITOR_SETUP_CMD="export EDITOR='vim'"
+PATH_SETUP_CMD="export PATH=$PATH:$HOME/bin/:$HOME/.bin/:$HOME/.local/bin/"
+
+echo_to_conf "$EDITOR_SETUP_CMD" 
+echo_to_conf "$PATH_SETUP_CMD" 
+
