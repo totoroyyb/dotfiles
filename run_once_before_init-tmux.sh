1979edc3d75bd710d80ae4b16fa77e3a13f75d43
Yibo Yan <yiboyan@usc.edu>
2024-01-13 21:37:14 -0700

    changes

--- run_once_before_init-tmux.sh
+++ run_once_before_init-tmux.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# Install xclip for clipboard support
 sudo apt-get update -y
 sudo apt-get install -y xclip
 
+# Install tmux config
 cd
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
+
+# Restart tmux server
+tmux kill-server
 
