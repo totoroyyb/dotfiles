11288c2ef398f2d97789d66ce093aa768b7beb47
Yibo Yan <yiboyan@usc.edu>
2024-02-21 02:02:41 -0700

    adjust scripts

--- run_once_after_setup-editor.sh
+++ run_once_after_setup-editor.sh
@@ -5,14 +5,15 @@
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
+rm -f nvim-linux64.tar.gz
 
-PATH="$PATH:/opt/nvim-linux64/bin"
+NVIM_BIN="/opt/nvim-linux64/bin"
 
-echo "export PATH=$PATH" >> "$HOME/.bashrc"
+echo "export PATH=\$PATH:$NVIM_BIN" >> "$HOME/.bashrc"
 source "$HOME/.bashrc"
 
 # Install LunarVim
 LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
 
-echo "export PATH=$HOME/.local/bin:$PATH" >> "$HOME/.bashrc"
+echo "export PATH=\$HOME/.local/bin:\$PATH" >> "$HOME/.bashrc"
 source "$HOME/.bashrc"
