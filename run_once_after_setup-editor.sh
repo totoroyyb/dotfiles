9732b5eaffb918658e5305d8d739a63358a80145
Yibo Yan <yiboyan@usc.edu>
2024-03-06 15:11:48 -0700

    disable lunarvim prompt

--- run_once_after_setup-editor.sh
+++ run_once_after_setup-editor.sh
@@ -8,12 +8,12 @@ sudo tar -C /opt -xzf nvim-linux64.tar.gz
 rm -f nvim-linux64.tar.gz
 
 NVIM_BIN="/opt/nvim-linux64/bin"
+export PATH=$NVIM_BIN:$PATH
 
 echo "export PATH=\$PATH:$NVIM_BIN" >> "$HOME/.bashrc"
-source "$HOME/.bashrc"
 
 # Install LunarVim
-LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh) -y
 
+export PATH=$HOME/.local/bin:$PATH
 echo "export PATH=\$HOME/.local/bin:\$PATH" >> "$HOME/.bashrc"
-source "$HOME/.bashrc"
