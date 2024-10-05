5d743d0345b9b2cdcbc4ddaad6b5ce15ee3c7f5f
Yibo Yan <yiboyan@usc.edu>
2024-10-05 20:34:08 +0000

    updates

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,5 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+-- Enable system clipboard support
+vim.opt.clipboard = "unnamedplus"
--- run_once_before_install-packages.sh
+++ run_once_before_install-packages.sh
@@ -2,6 +2,14 @@
 
 OS=$(uname -s)
 
+# lazyvim dependencies
+sudo apt-get install -y unzip
+
+# # installs nvm (Node Version Manager)
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+# # download and install Node.js (you may need to restart the terminal)
+# nvm install 20
+
 DOWNLOAD_FOLDER=/tmp/
 
 # rustup + cargo
@@ -11,18 +19,38 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y -q
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-# delta
-cargo install git-delta
+# delta (diff)
+cargo install -q git-delta
+
+# tldr
+cargo install -q tlrc
+
+# eza (ls)
+cargo install eza
+
+# zoxide (cd)
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+
+# pyenv
+curl https://pyenv.run | bash
 
 # Install gh
 if [ "$OS" = "Darwin" ]; then
-  # bat
+  # bat (cat)
   brew install bat
+
+  # fuck
+  # brew install thefuck
 elif [ "$OS" = "Linux" ]; then
-  # bat
+  # bat (cat)
   sudo apt install bat
   mkdir -p ~/.local/bin
   ln -s /usr/bin/batcat ~/.local/bin/bat
+
+  # fuck
+  # sudo apt-get update
+  # sudo apt-get -y install python3-dev python3-pip python3-setuptools
+  # pip3 install thefuck --user --break-system-packages
 else
   echo "NOT MACOS/LINUX: abort package installation."
   exit
